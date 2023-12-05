de67dd1596e1deff7b3706f5dffeea0b809b6c8d
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-12-05 10:49:33 +0100

    adding posts

--- client/src/scenes/homePage/index.jsx
+++ client/src/scenes/homePage/index.jsx
@@ -1,13 +1,34 @@
-import { Box } from '@mui/material'
-import React from 'react'
-import NavBar from 'scenes/navbar'
+import { Box, useMediaQuery } from "@mui/material";
+import React from "react";
+import { useSelector } from "react-redux";
+import NavBar from "scenes/navbar";
+import UserWidget from "scenes/widgets/UserWidget";
+import MyPostWidget from "scenes/widgets/MyPostWidget"
 
 const HomePage = () => {
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const { _id, picturePath } = useSelector((state) => state.user);
   return (
     <Box>
       <NavBar />
+      <Box
+        width="100%"
+        padding="2rem 6%"
+        display={isNonMobileScreens ? "flex" : "block"}
+      >
+        <Box flexBasis={isNonMobileScreens ? "26%" : undefined}>
+          <UserWidget userId={_id} picturePath={picturePath} />
+        </Box>
+        <Box
+          flexBasis={isNonMobileScreens ? "42%" : undefined}
+          mt={isNonMobileScreens ? undefined : "2rem"}
+        >
+          <MyPostWidget picturePath={picturePath} />
+        </Box>
+        {isNonMobileScreens && <Box flexBasis="26%"> </Box>}
+      </Box>
     </Box>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
