7fbd4711149408cb906927cf6eb6da5c7dc00065
Elijah Aremu <aremuelijah.ae@gmail.com>
2023-12-06 15:30:21 +0100

    post widget

--- client/src/scenes/widgets/MyPostWidget.jsx
+++ client/src/scenes/widgets/MyPostWidget.jsx
@@ -1,48 +1,77 @@
-import { useTheme } from "@emotion/react"
+import { useTheme } from "@emotion/react";
 import {
-    EditOutlined,
-    DeleteOutlined,
-    AttachFileOutlined,
-    GifBoxOutlined,
-    ImageOutlined,
-    MicOutlined,
-    MoreHorizOutlined
-} from "@mui/icons-material"
-import { Box, Divider, Typography, useMediaQuery, useTheme } from "@mui/material"
-import FlexBetween from "components/FlexBetween"
-import UserImage from "components/UserImage"
-import widgetWrapper from "components/WidgetWrapper"
-import { useState } from "react"
-import Dropzone from "react-dropzone"
-import { useDispatch, useSelector } from "react-redux"
-import { setPosts } from "state"
+  EditOutlined,
+  DeleteOutlined,
+  AttachFileOutlined,
+  GifBoxOutlined,
+  ImageOutlined,
+  MicOutlined,
+  MoreHorizOutlined,
+} from "@mui/icons-material";
+import { Box, Divider, Typography, useMediaQuery } from "@mui/material";
+import FlexBetween from "components/FlexBetween";
+import UserImage from "components/UserImage";
+import WidgetWrapper from "components/WidgetWrapper";
+import { useState } from "react";
+import Dropzone from "react-dropzone";
+import { useDispatch, useSelector } from "react-redux";
+import { setPosts } from "state";
 
+const MyPostWidget = ({ picturePath }) => {
+  const dispatch = useDispatch();
+  const [isImage, setIsImage] = useState(false);
+  const [image, setImage] = useState(null);
+  const [post, setPost] = useState("");
+  const { palette } = useTheme();
+  const { _id } = useSelector((state) => state.user);
+  const token = useSelector((state) => state.token);
+  const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
+  const mediumMain = palette.neutral.mediumMain;
+  const medium = palette.neutral.medium;
 
-const MyPostWidget = ({ picturePath}) => {
-    const dispatch = useDispatch()
-    const [isImage, setIsImage] = useState(false)
-    const [image, setImage] = useState(null)
-    const [post, setPost] = useState("")
-    const { palette } = useTheme()
-    const { _id } = useSelector((state)=> state.user)
-    const token = useSelector((state)=> state.token)
-    const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
-    const mediumMain = palette.neutral.mediumMain
-    const medium = palette.neutral.medium
-
-    const handlePost = async () => {
-        const formData = new formData()
-        formData.append("userId", _id)
-        formData.append("description", post)
-        if (image) {
-            formData.append("picture", image)
-            formData.append("picturePath", image.name)
-        }
-
-        const response = await fetch('http://localhost:3001/posts', {
-            method: 'POST',
-            headers : { Authorization: `Bearer ${token}`},
-            body : formData
-        })
+  const handlePost = async () => {
+    const formData = new formData();
+    formData.append("userId", _id);
+    formData.append("description", post);
+    if (image) {
+      formData.append("picture", image);
+      formData.append("picturePath", image.name);
     }
-}
+
+    const response = await fetch("http://localhost:3001/posts", {
+      method: "POST",
+      headers: { Authorization: `Bearer ${token}` },
+      body: formData,
+    });
+    const posts = await response.json();
+    dispatch(setPosts({ posts }));
+    setImage(null);
+    setPost("");
+  };
+
+  return (
+    <WidgetWrapper>
+      <FlexBetween gap="1.5rem">
+        <UserImage image={picturePath} />
+        <InputBase
+          placeholder="What's on your mind"
+          onChange={(e) => setPost(e.target.value)}
+          value={post}
+          sx={{
+            width: "100%",
+            backgroundColor: palette.neutral.light,
+            borderRadius: "2rem",
+            padding: "1rem 2rem",
+          }}
+        />
+      </FlexBetween>
+      {isImage && (
+         <Box border={`1px solid ${medium}`}>
+
+         </Box>
+      )}
+    </WidgetWrapper>
+  );
+};
+
+export default MyPostWidget;
